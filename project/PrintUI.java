3e7c2c57844540bbdaf5c2dc8d83cc53ab5348c2
HJK9810 <ruby45342@gmail.com>
2022-03-22 16:44:51 +0900

    sorted

--- project/PrintUI.java
+++ project/PrintUI.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package project;
 
 import java.util.ArrayList;
@@ -74,95 +73,28 @@ public class PrintUI {
 
 			if (gender.equals("W")) {
 				System.out.print("나이를 입력하세요 : ");
-				return (scanner.nextInt() / 10) + "0대여성";
+				int num = scanner.nextInt();
+				if(num / 10 < 1) continue;
+				return (num / 10) + "0대여성";
 			} else if (gender.equals("M")) {
 				System.out.print("나이를 입력하세요 : ");
-				return (scanner.nextInt() / 10) + "0대남성";
-			} else System.out.println("\n다시 입력하세요.");
+				int num = scanner.nextInt();
+				if(num / 10 < 1) continue;
+				return (num / 10) + "0대남성";
+			} else System.out.println("다시 입력하세요.");
 		}
 	}
-=======
-package project;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class PrintUI {
-	Scanner scanner = new Scanner(System.in);
-
-	protected String printMonth() {
-		String month = "";
-
-		while (true) {
-			System.out.println("Whitch month do you want to check?");
+	
+	protected int sorted() {
+		while(true) {
+			System.out.println("어떤 정렬을 사용하겠습니까?");
+			System.out.println("1. 내림차순\n2. 오름차순");
 			int num = scanner.nextInt();
-
-			if (num < 10 && num > 0) {
-				month = "0" + num;
-				break;
-			} else if (num < 13) {
-				month = num + "";
-				break;
-			} else System.out.println("Out of order. One more to push another month.");
-		}
-
-		return month;
-	}
-
-	protected String printTowns() {
-		ArrayList<String> fullTowns1 = new ArrayList<>();
-		ArrayList<String> fullTowns2 = new ArrayList<>();
-
-		ArrayList<String> count1 = new ArrayList<>();
-		ArrayList<String> count2 = new ArrayList<>();
-		String[] ary1 = { "서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종" };
-		String[] ary2 = { "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주" };
-
-		for (int i = 0; i < ary1.length; i++) {
-			fullTowns1.add(ary1[i] + "\t");
-			count1.add(i + "\t");
-		}
-		for (int i = 0; i < ary1.length; i++) {
-			fullTowns2.add(ary2[i] + "\t");
-			count2.add(i + "\t");
-		}
-
-		int num = 0;
-		while (true) {
-			System.out.println("원하는 출발지를 선택하세요.");
-			System.out.println("1. 특별자치시\n2. 도");
-			num = scanner.nextInt();
-			if (num == 1) {
-				System.out.println(fullTowns1);
-				System.out.println(count1);
-
-				System.out.println("원하는 행정구역의 번호를 선택하세요.(해당 번호는 아래 위치한 번호입니다.)");
-				return ary1[scanner.nextInt()];
-			} else if (num == 2) {
-				System.out.println(fullTowns2);
-				System.out.println(count2);
-
-				System.out.println("원하는 행정구역의 번호를 선택하세요.(해당 번호는 아래 위치한 번호입니다.)");
-				return ary2[scanner.nextInt()];
-			} else {
-				System.out.println("Out of order. Error. chose one more!");
-			}
+			
+			if(num != 1 || num != 2) {
+				System.out.println("다시 입력하세요.");
+				continue;
+			} else return num;
 		}
 	}
-
-	protected String printGAndY() {
-		while (true) {
-			System.out.print("성별을 입력하세요(M - 남성 / W - 여성) : ");
-			String gender = scanner.next().toUpperCase();
-
-			if (gender.equals("W")) {
-				System.out.print("나이를 입력하세요 : ");
-				return (scanner.nextInt() / 10) + "0대여성";
-			} else if (gender.equals("M")) {
-				System.out.print("나이를 입력하세요 : ");
-				return (scanner.nextInt() / 10) + "0대남성";
-			} else System.out.println("\n다시 입력하세요.");
-		}
-	}
->>>>>>> fba2d64d13719762213b7c8513fd979426c6bc1b
 }
--- project/floatpop.java
+++ project/floatpop.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package project;
 
 import java.io.BufferedReader;
@@ -6,9 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class floatpop {
 
@@ -23,6 +21,10 @@ public class floatpop {
 			BufferedReader br = new BufferedReader(new FileReader("C:\\javatest\\시군별유동인구데이터.csv"));
 			String line;
 			int idx = -1;
+			HashMap<String, String[]> list = new HashMap<>();
+			HashMap<String, Float> sorted = new HashMap<>();
+			
+			String start = "";
 
 			while ((line = br.readLine()) != null) {
 				// date, incode, in, outcode, out, man 10~70, woman 10~70
@@ -35,6 +37,7 @@ public class floatpop {
 							break;
 						}
 					}
+					start = ary[2];
 					fw.write(ary[0] + "," + ary[2] + "," + ary[4] + ", " + yandg + "\n");
 					continue;
 				}
@@ -42,8 +45,19 @@ public class floatpop {
 				if (!month.equals(ary[0].replace(" ", "").substring(4))) continue;
 
 				if (!ary[1].equals(ary[3]) && ary[2].contains(towns)) {
+					if(ary[2].equals(start)) {
+						sorted.put(ary[4], Float.parseFloat(ary[idx]));
+					} else {
+						List<Map.Entry<String, Float>> entries = new ArrayList<>(sorted.entrySet());
+						Collections.sort(entries, Comparator.comparing(Map.Entry::getValue));
+						List<String> changelist = changeList(entries);
+						String[] arr = new String[changelist.size()];
+												
+						list.put(start, arr);
+						start = ary[2];
+					}
+						
 					String str = month + "월," + ary[2] + "," + ary[4] + ", " + ary[idx];
-
 					fw.write(str + "\n");
 				}
 			}
@@ -55,63 +69,14 @@ public class floatpop {
 			e.printStackTrace();
 		}
 	}
-}
-=======
-package project;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class floatpop {
-
-	public static void ReWritecsv() throws IOException {
-		FileWriter fw = new FileWriter("C:\\javatest\\시군유동인구.txt", false);
-		PrintUI pui = new PrintUI();
-		String month = pui.printMonth();
-		String towns = pui.printTowns();
-		String yandg = pui.printGAndY();
+	
+	public static List<String> changeList(List<Map.Entry<String, Float>> map) {
+		List<String> list = new ArrayList<>();
 		
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("C:\\javatest\\시군별유동인구데이터.csv"));
-			String line;
-			int idx = -1;
-
-			while ((line = br.readLine()) != null) {
-				// date, incode, in, outcode, out, man 10~70, woman 10~70
-				String[] ary = line.split(",");
-				
-				if (ary[0].equals("년월")) {
-					for(int i=0;i<ary.length;i++) {
-						if(ary[i].contains(yandg)) {
-							idx = i;
-							break;
-						}
-					}
-					fw.write(ary[0] + "," + ary[2] + "," + ary[4] + ", " + yandg + "\n");
-					continue;
-				}
-				if (ary.length < 5 || ary[0].replace(" ", "").length() < 6) continue;
-				if (!month.equals(ary[0].replace(" ", "").substring(4))) continue;
-
-				if (!ary[1].equals(ary[3]) && ary[2].contains(towns)) {
-					String str = month + "월," + ary[2] + "," + ary[4] + ", " + ary[idx];
-
-					fw.write(str + "\n");
-				}
-			}
-			fw.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		for(Map.Entry<String, Float> entry : map) {
+			list.add(entry.getKey() +  "-" + entry.getValue());
 		}
+		
+		return list;
 	}
 }
->>>>>>> fba2d64d13719762213b7c8513fd979426c6bc1b
