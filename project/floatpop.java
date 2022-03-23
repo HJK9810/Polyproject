4ef122175ca0e6c87a564e606bb9c45a7e624ca7
HJK9810 <ruby45342@gmail.com>
2022-03-23 13:45:04 +0900

    add motation

--- project/floatpop.java
+++ project/floatpop.java
@@ -11,67 +11,75 @@ import java.util.Map.Entry;
 public class floatpop {
 	protected static HashMap<String, String[]> list = new HashMap<>();
 	protected static ArrayList<String> maintowns = new ArrayList<>();
-	protected static String homeTown = "";
 
-	public static void ReWritecsv() throws IOException {
+	public void ReWritecsv() throws IOException {
 		PrintUI pui = new PrintUI();
+		floatpop fp = new floatpop();
 		String month = pui.printMonth();
 		String towns = pui.printTowns();
 		String yandg = pui.printGAndY();
-		FileWriter fw = new FileWriter("C:\\javatest\\" + month + "월" + towns + "시군유동인구.txt", false);
+		// for rewrite
+		FileWriter fw = new FileWriter("C:\\javatest\\" + month + "월" + towns + "시군" + yandg + "대유동인구.csv", false);
 
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("C:\\javatest\\시군별유동인구데이터.csv"));
 			String line;
 			int idx = -1;
 
-			HashMap<String, Float> sorted = new HashMap<>();
+			HashMap<String, Float> sorted = new HashMap<>(); // string for in & float for people
 
-			String start = "";
+			String start = ""; // where to arrive
 
 			while ((line = br.readLine()) != null) {
 				// date, incode, in, outcode, out, man 10~70, woman 10~70
 				String[] ary = line.split(",");
 
-				if (ary[0].equals("년월")) {
+				if (ary[0].equals("년월")) { // the first line
 					for (int i = 0; i < ary.length; i++) {
-						if (ary[i].contains(yandg)) {
+						if (ary[i].contains(yandg)) { // check index which want to check age
 							idx = i;
 							break;
 						}
 					}
-					start = ary[2];
+					start = ary[2]; // set where to arrive
 					fw.write(ary[0] + "," + ary[2] + "," + ary[4] + ", " + yandg + "\n");
 					continue;
 				}
 				if (ary.length < 5 || ary[0].replace(" ", "").length() < 6) continue;
 				if (!month.equals(ary[0].replace(" ", "").substring(4))) continue;
 
-				if (!ary[1].equals(ary[3]) && ary[2].contains(towns)) {
+				// check which want to from big cities && not same town to same town
+				if (ary[2].contains(towns) && !ary[1].equals(ary[3])) { 
 					if (ary[2].equals(start)) {
 						sorted.put(ary[4], Float.parseFloat(ary[idx]));
 					} else {
-						List<Map.Entry<String, Float>> entries = new ArrayList<>(sorted.entrySet());
-						entries.sort(Map.Entry.comparingByValue());
-						ArrayList<String> changeList = new ArrayList<>();
-						
-						for(Map.Entry<String, Float> entry : entries) {
-							changeList.add(entry.getKey() + "-" + entry.getValue());
-						}
-						String[] arr = changeList.toArray(new String[changeList.size()]);
-						
-						
+						String[] arr = fp.mapsorted(sorted);
+
 						String[] tstart = start.split(" ");
-						
-						if(tstart.length == 1) {
+
+						if (tstart.length == 1) { // for the first line
 							list.put(start, arr);
 							maintowns.add(start);
 						} else {
-							homeTown = tstart[0];
 							list.put(tstart[1], arr);
 							maintowns.add(tstart[1]);
 						}
+						
 						start = ary[2];
+						
+						if(list.containsKey(start)) { // if same towns?
+							String[] array = list.get(start);
+							sorted = new HashMap<>();
+							
+							for (String str : array) {
+								String[] formap = str.split("-");
+								sorted.put(formap[0].trim(), Float.parseFloat(formap[1].trim()));
+							}
+						} else {
+							sorted = new HashMap<>();
+						}
+
+						sorted.put(ary[4], Float.parseFloat(ary[idx]));
 					}
 
 					String str = month + "월," + ary[2] + "," + ary[4] + ", " + ary[idx];
@@ -85,5 +93,18 @@ public class floatpop {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected String[] mapsorted(HashMap<String, Float> map) {
+		List<Map.Entry<String, Float>> entries = new ArrayList<>(map.entrySet());
+		entries.sort(Map.Entry.comparingByValue()); // hashmap sort
+		ArrayList<String> changeList = new ArrayList<>(); // set arraylist for change array
+
+		for (Map.Entry<String, Float> entry : entries) { // input key and value to string
+			changeList.add(entry.getKey() + "-" + entry.getValue());
+		}
+		String[] ary = changeList.toArray(new String[changeList.size()]); // arraylist to array
+
+		return ary;
 	}
 }
