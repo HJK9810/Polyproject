b70904be1c447be43d6e80387c87f8590793b5b9
HJK9810 <ruby45342@gmail.com>
2022-03-24 09:51:10 +0900

    seperate 0

--- project/checkAll.java
+++ project/checkAll.java
@@ -8,9 +8,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class checkAll {
 	public void printAll() {
+		Scanner scanner = new Scanner(System.in);
 		floatpop fp = new floatpop();
 		checkAll cA = new checkAll();
 		String path = fp.path;
@@ -38,7 +40,9 @@ public class checkAll {
 
 		ArrayList<String> townlist = cA.compare(towns);
 
-		System.out.println("전체에서의 유동인구");
+		System.out.println("If print worst, do you want to delet 0.0?(Yes input >0, No input <= 0)");
+		int num = scanner.nextInt();
+		System.out.println("\n전체에서의 유동인구");
 		System.out.println("Top 10");
 		for (int i = 0; i < 10; i++) {
 			int listsize = townlist.size() - 1;
@@ -46,10 +50,29 @@ public class checkAll {
 			System.out.println((i + 1) + ". " + ary[0] + " 에서 " + ary[1] + ":" + ary[2]);
 		}
 
-		System.out.println("Worts 10");
-		for (int i = 0; i < 10; i++) {
-			String[] ary = townlist.get(i).split("-");
-			System.out.println((i + 1) + ". " + ary[0] + " 에서 " + ary[1] + ":" + ary[2]);
+		System.out.println("\nWorts 10");
+		if (num <= 0) {
+			for (int i = 0; i < 10; i++) {
+				String[] ary = townlist.get(i).split("-");
+				System.out.println((i + 1) + ". " + ary[0] + " 에서 " + ary[1] + ":" + ary[2]);
+			}
+		} else {
+			int j = 0;
+			ArrayList<String> zeros = new ArrayList<>();
+			for (int i = 0; i < 10 + j; i++) {
+				String[] ary = townlist.get(i + j).split("-");
+				if (ary[2].equals("0.0")) {
+					zeros.add(ary[0] + " 에서 " + ary[1]);
+					j++;
+				} else {
+					if(i == townlist.size() - 1) break;
+					System.out.println((i + 1 - j) + ". " + ary[0] + " 에서 " + ary[1] + ":" + ary[2]);
+				}
+			}
+			System.out.println("\nThey are 0.0 cities : " + zeros.size());
+			for(String str : zeros) {
+				System.out.println(str);
+			}
 		}
 	}
 
