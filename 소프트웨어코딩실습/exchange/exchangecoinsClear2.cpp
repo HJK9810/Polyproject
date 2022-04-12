d665b390e11b8b901fff09fb80e2b2427c0877bf
HJK9810 <ruby45342@gmail.com>
2022-04-12 16:32:45 +0900

    add exchange sepear

--- 소프트웨어코딩실습/exchange/exchangecoinsClear2.cpp
+++ 소프트웨어코딩실습/exchange/exchangecoinsClear2.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void changeCoins(int money) {
 	const int counts[5] = {1000, 500, 100, 50, 10};
@@ -10,7 +11,24 @@ void changeCoins(int money) {
 		if(coin != 0) printf("%d원: %d 개. ", counts[i], coin);
 	}
 	
-	printf("\n");
+	printf("\n\n");
+}
+
+void changeExchan(int money, char unit[], int input) {
+	const int counts[9] = {5000, 1000, 500, 100, 50, 20, 10, 5, 1};
+	
+	for(int i = 0; i < 9; i++) {
+		if(input != 2 && i < 3) continue; // 엔화만 5000, 1000, 500엔 존재 
+		if(input == 5 && i == 3) continue; // 파운드는 100파운드 이상 존재X 
+		if(input == 2 && i == 5) continue; // 엔화만 20엔 없음 
+		
+		int coin = money / counts[i];
+		money -= coin * counts[i];
+		
+		if(coin != 0) printf("%d%s: %d 개. ", counts[i], unit, coin);
+	}
+	
+	printf("\n\n");
 }
 
 int main() {
@@ -35,9 +53,14 @@ int main() {
 	
 	printf("기준 환율 : %.2f\n\n", rate); // 모든 환율이 소수점 둘째짜리까지기에 .2f 사용 
 	printf("환전결과\n");
-	printf("%d 원 -> %d %s\n", inputWon, (int) output, UNIT[input - 1]); // 환전 출력 
+	printf("%d 원 -> %d %s\n", inputWon, (int) output, UNIT[input - 1]); // 환전 출력
+	char unit[10];
+	strcpy(unit, UNIT[input - 1]);
+	
+	changeExchan((int) output, unit, input);
+	
 	printf("거스름돈 : %d 원\n", change / 10 * 10); // 거스름돈 출력 - 1원단위 버림 
-	printf("1원단위시 거스름돈 : %d 원\n\n", change); // 거스름돈 출력 - 1원단위까지 출력 
+	printf("1원단위시 거스름돈 : %d 원\n", change); // 거스름돈 출력 - 1원단위까지 출력 
 	changeCoins(change);
 	
 	return 0;
