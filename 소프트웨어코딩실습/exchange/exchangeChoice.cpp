76999f4d88f817f9dd3c0dcdad98c2624904edae
HJK9810 <ruby45342@gmail.com>
2022-04-12 10:54:25 +0900

    seperate cal & use switch

--- 소프트웨어코딩실습/exchange/exchangeChoice.cpp
+++ 소프트웨어코딩실습/exchange/exchangeChoice.cpp
@@ -14,7 +14,9 @@ int main() {
 	int return100;
 	int return50;
 	int return10;
-	int newChange = 0; // 동전계산을 위한 거스름돈 
+	int newChange = 0; // 동전계산을 위한 거스름돈
+	float rate = 0.0;
+	const char UNIT[][10] = {"달러", "엔", "유로", "위안", "파운드"};
 	
 	printf("환전을 원하는 금액을 입력하세요(원화) : ");
 	scanf("%d", &inputWon);
@@ -23,42 +25,29 @@ int main() {
 	scanf("%d", &input);
 	printf("\n");
 	
-	if(input == 1) { // USD
-		float outputUSD = inputWon / RATE_USD; // 환전 = 바꾸는 원화 / 환율
-		int finalOutputUSD = outputUSD; 
-		change = (int) (outputUSD * 100) % 100 * RATE_USD / 100; // 거스름돈
-		
-		printf("USD 환전\n");
-		printf("%d 원 -> %d 달러\n", inputWon, finalOutputUSD); // 환전 출력 
-	} else if(input == 2) { // JPY
-		float outputJPY = inputWon / RATE_JPY; // 환전 = 바꾸는 원화 / 환율
-		int finalOutputJPY = outputJPY; 
-		change = (int) (outputJPY * 100) % 100 * RATE_JPY; // 거스름돈
-			
-		printf("JPY 환전\n");
-		printf("%d 원 -> %d 엔\n", inputWon, finalOutputJPY); // 환전 출력 
-	} else if(input == 3) { // EUR
-		float outputEUR = inputWon / RATE_EUR; // 환전 = 바꾸는 원화 / 환율
-		int finalOutputEUR = outputEUR; 
-		change = (int) (outputEUR * 100) % 100 * RATE_EUR / 100; // 거스름돈
-			
-		printf("EUR 환전\n");
-		printf("%d 원 -> %d 유로\n", inputWon, finalOutputEUR); // 환전 출력 
-	} else if (input == 4) { // CNY
-		float outputCNY = inputWon / RATE_CNY; // 환전 = 바꾸는 원화 / 환율
-		int finalOutputCNY = outputCNY; 
-		change = (int) (outputCNY * 100) % 100 * RATE_CNY / 100; // 거스름돈
-			
-		printf("CNY 환전\n");
-		printf("%d 원 -> %d 위안\n", inputWon, finalOutputCNY); // 환전 출력 
-	} else if(input == 5) { // GBP
-		float outputGBP = inputWon / RATE_GBP; // 환전 = 바꾸는 원화 / 환율
-		int finalOutputGBP = outputGBP; 
-		change = (int) (outputGBP * 100) % 100 * RATE_GBP / 100; // 거스름돈
-		
-		printf("GBP 환전\n");
-		printf("%d 원 -> %d 파운드\n", inputWon, finalOutputGBP); // 환전 출력 
+	switch(input) {
+		case 1: // USD
+			rate = RATE_USD;
+			break;
+		case 2: // JPY
+			rate = RATE_JPY;
+			break;
+		case 3: // EUR
+			rate = RATE_EUR;
+			break;
+		case 4: // CNY
+			rate = RATE_CNY; 
+			break;
+		case 5: // GBP
+			rate = RATE_GBP;
+			break;
+		default:
+			break;
 	}
+	
+	float output = inputWon / rate; // 환전 = 바꾸는 원화 / 환율
+	int finalOutput = output; 
+	change = (int) (output * 100) % 100 * rate / 100; // 거스름돈
 	
 	newChange = change;
 	return1000 = newChange / 1000;
@@ -70,7 +59,8 @@ int main() {
 	return50 = newChange / 50;
 	newChange -= return50 * 50;
 	return10 = newChange / 10;
-	
+	printf("환전결과\n");
+	printf("%d 원 -> %d %s\n", inputWon, finalOutput, units[input - 1]); // 환전 출력 
 	printf("거스름돈 : %d 원\n\n", change / 10 * 10); // 거스름돈 출력
 	printf("1000원 %d개. 500원 %d개. 100원 %d개. 50원 %d개. 10원 %d개.", return1000, return500, return100, return50, return10);
 	
