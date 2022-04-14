a5e5350fa57ee81aafd08a667b7135462b139e4e
HJK9810 <ruby45342@gmail.com>
2022-04-14 09:31:14 +0900

    seperate func

--- 소프트웨어코딩실습/loop/printCal2.cpp
+++ 소프트웨어코딩실습/loop/printCal2.cpp
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+int calcul(int year, int month) {
+	int dates[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // idx = 각 달 => 윤년기준, 1년 모든달 날짜수
+	int count = year - 1900; // 1900년대 이후만 가능한 계산 
+	int yearSum = 0; // 입력 전년도까지 지난 총 날짜수 합 
+		
+	for(int idx = 0; idx < count; idx++) { // yearsum = 1900년을 기준으로 입력 년도까지 촣 몇일이 지났는지 계산 
+		int checkY = 1900 + idx; // 윤년을 알아보기 위한 세팅 
+			
+		// 윤년의 조건 : 4의 배수이자 100의 배수가 아닐때 or 400의 배수일때
+		// 윤년 = 1년 366일, 평년 = 1년 365일 
+		if (checkY % 4 == 0 && checkY % 100 != 0) yearSum += 366;
+		else if(checkY % 400 == 0) yearSum += 366;
+		else {
+			yearSum += 365;
+			dates[2] = 28; // 배열 초기화가 윤년기준이기에 
+		}
+	}
+		
+	int dateSum = 0; // dateSum = 1월을 기준으로 입력 달 까지 총 몇일이 지났는지 계산 
+	for(int idx = 1; idx < month; idx++) {
+		dateSum += dates[idx];
+	}
+	int day =  (yearSum + dateSum + 1) % 7; // 요일 계산 = 1주일은 7일 => 0 = 일, 1 = 월.... 
+	
+	return dates[month] * 10 + day; // days = 마지막날 * 10 + 요일(0~6) 
+}
+
 void printUI(int day, int date) { // 출력 - day : 요일, date : 마지막날짜 
 	int today = 1; // for 날짜
 	
@@ -28,8 +55,7 @@ int main() {
 	int year = 1900; // 컴퓨터 기본 년도 
 	int month = 1;	// 컴퓨터 기본 월 
 	
-	while(true) { // 무한루프 
-		int dates[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // idx = 각 달 => 윤년기준, 1년 모든달 날짜수 
+	while(true) { // 무한루프  
 		printf("  년도 입력(1900년대 이후만 가능합니다. 0은 종료입니다.) : ");
 		scanf("%d", &year);
 		
@@ -42,34 +68,10 @@ int main() {
 		if (month < 1 || month > 13) continue; // 범위 이내일경우, 재입력 
 		
 		printf("\n\n");	
+		int days = calcul(year, month); // days = 마지막날 * 10 + 요일(0~6)
 		
-		int count = year - 1900; // 1900년대 이후만 가능한 계산 
-		int yearSum = 0; // 입력 전년도까지 지난 총 날짜수 합 
-		
-		for(int idx = 0; idx < count; idx++) { 
-			int checkY = 1900 + idx; // 윤년을 알아보기 위한 세팅 
-			
-			// 윤년의 조건 : 4의 배수이자 100의 배수가 아닐때 or 400의 배수일때
-			// 윤년 = 1년 366일, 평년 = 1년 365일 
-			if (checkY % 4 == 0 && checkY % 100 != 0) yearSum += 366;
-			else if(checkY % 400 == 0) yearSum += 366;
-			else {
-				yearSum += 365;
-				dates[2] = 28; // 배열 초기화가 윤년기준이기에 
-			}
-		}
-		
-		int dateSum = 0;
-		for(int idx = 1; idx < month; idx++) {
-			dateSum += dates[idx];
-		}
-		
-		// yearsum = 1900년을 기준으로 입력 년도까지 촣 몇일이 지났는지 계산 
-		// dateSum = 1월을 기준으로 입력 달 까지 총 몇일이 지났는지 계산 
-		int day =  (yearSum + dateSum + 1) % 7; // 요일 계산 = 1주일은 7일 => 0 = 일, 1 = 월.... 
-
 		printf("===================== %d년 %d월 =======================\n", year, month);
-		printUI(day, dates[month]); // 출력을 위한 함수 - 요일, 해당달 마지막날 
+		printUI(days % 10, days / 10); // 출력을 위한 함수 - 요일, 해당달 마지막날 
 	}
 
 	return 0;
